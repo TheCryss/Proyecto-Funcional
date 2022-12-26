86e00f3e4209cc01042cb133c3ebddb7b5faffe2
GetialJuan <getial.juan@correounivalle.edu.co>
2022-12-26 11:55:13 -0500

    Primeros dataTypes

--- SimuladorDePolarizacionEnRedes/src/main/scala/dataTypes/package.scala
+++ SimuladorDePolarizacionEnRedes/src/main/scala/dataTypes/package.scala
@@ -1,3 +1,10 @@
 package object dataTypes {
+  // Tipo para los valores, reales, de una distribucion
+  // Contiene cualquier valor real
+  type DistributionValues = Vector[Double]
 
+  // pi_k es una frecuencia de longitud k
+  // si pi_k.lenght = k, 0 <= pi_k(i) <= 1, 0 <= i <= k-1
+  // pi_k.sum == 1
+  type Frequency = Vector[Double] //Valores entre 0 y 1
 }
