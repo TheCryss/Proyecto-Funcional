d7a6290f9613cfb09b710cebec86d60d153dfd31
GetialJuan <getial.juan@correounivalle.edu.co>
2022-12-26 12:32:04 -0500

    funcion rhoER lista

--- SimuladorDePolarizacionEnRedes/src/main/scala/functions/package.scala
+++ SimuladorDePolarizacionEnRedes/src/main/scala/functions/package.scala
@@ -1,3 +1,15 @@
 package object functions {
+  // dataTypes
+  import dataTypes._
 
+  // rhoER
+  def rhoER(d:Distribution):Double = {
+    // d._1:Pi  d._2:Y
+    // K = 10
+    val alfa = 1.6
+    val k = d._2.length
+    val sumRes = for (i <- (0 until k); j <- (0 until k)) yield
+      Math.pow(d._1(i), 1+alfa) * d._1(j) * (d._2(i) - d._2(j)).abs
+    10 * sumRes.sum
+  }
 }
